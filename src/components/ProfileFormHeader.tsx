d415905eb42001d93ee1f178eef1413df9666c08
Reem Ben-David <reemile1996@gmail.com>
2025-07-16 00:09:22 +0300

    Extract ProfileFormHeader to a component

---
+++ src/components/ProfileFormHeader.tsx
@@ -0,0 +1,3 @@
+export const ProfileFormHeader = () => <header className="bg-gradient-to-r from-blue-500 to-blue-700 rounded-t-xl -mt-8 -mx-8 mb-8 p-6 shadow text-white flex flex-col items-center">
+    <h2 className="font-bold text-2xl tracking-tight drop-shadow mb-0">Tell us about yourself...</h2>
+</header>
