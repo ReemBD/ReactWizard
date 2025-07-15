b38c6efa14872c5ae13e0d1661dcee3fa0b771b4
Reem Ben-David <reemile1996@gmail.com>
2025-07-15 16:30:51 +0300

    Render step

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Step } from './wizard/Step'
+import { Wizard } from './wizard/Wizard.context'
 
 function App() {
 
   return (
     <div>
-   
+      <Wizard>
+        <Step>Hello world</Step>
+      </Wizard>
     </div>
   )
 }
